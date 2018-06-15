91a554523700620849a2a1635d06892da7f05d31
taras-d <taras.datsenko@gmail.com>
2018-06-15 15:28:34 +0300

    Skip duplicated words before insert

--- src/services/words/words.hooks.js
+++ src/services/words/words.hooks.js
@@ -1,17 +1,70 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { disableMultiItemChange, disallow } = require('feathers-hooks-common');
+const errors = require('@feathersjs/errors');
+const _ = require('lodash');
+
+const findWord = (text, model) => {
+  return model.findOne({
+    text: new RegExp(`^${text}$`, 'i')
+  }).exec();
+};
+
+const skipDuplicates = async context => {
+  const models = context.app.get('mongooseClient').models;
+  const words = Array.isArray(context.data)? context.data: [context.data];
+  const unique = [];
+
+  for (let i = 0; i < words.length; ++i) {
+    const word = words[i];
+    if (
+      !unique.find(w => _.toLower(w.text) === _.toLower(word.text)) &&
+      !await findWord(word.text, models.words)
+    ) {
+      unique.push(word);
+    }
+  }
+
+  context.data = unique;
+
+  const result = {
+    inserted: unique.length,
+    duplicates: words.length - unique.length
+  };
+
+  if (unique.length) {
+    context.dispatch = result;
+  } else {
+    context.result = result;
+  }
+};
+
+const checkWordBeforePatch = async context => {
+  const data = context.data;
+
+  if ('text' in data) {
+    const models = context.app.get('mongooseClient').models;
+    const word = await findWord(data.text, models.words);
+
+    if (word && word.id !== context.id) {
+      throw new errors.BadRequest(`Word with text "${data.text}" already exist`);
+    }
+  }
+};
 
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [],
+    create: [
+      skipDuplicates
+    ],
     update: [
       disallow('external')
     ],
     patch: [
-      disableMultiItemChange()
+      disableMultiItemChange(),
+      checkWordBeforePatch
     ],
     remove: [
       disableMultiItemChange()
