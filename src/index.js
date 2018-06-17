0c44d7640b3e57729ae712fe9e0360e3ca7c7f75
taras-d <taras.datsenko@gmail.com>
2018-06-17 22:52:36 +0300

    Open shift port/ip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 const logger = require('winston');
 const app = require('./app');
-const port = process.env.PORT || app.get('port');
-const server = app.listen(port);
+
+const port = process.env.OPENSHIFT_NODEJS_PORT || app.get('port');
+const ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const server = app.listen(port, ip);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
