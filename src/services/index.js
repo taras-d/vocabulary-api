0aec577825c5c3976344edf8e3cfb770938f234b
taras-d <taras.datsenko@gmail.com>
2018-07-08 22:07:49 +0300

    Words stats service

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const me = require('./me/me.service.js');
 const words = require('./words/words.service.js');
 const randomWord = require('./random-word/random-word.service.js');
+const wordsStats = require('./words-stats/words-stats.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(me);
   app.configure(words);
   app.configure(randomWord);
+  app.configure(wordsStats);
 };
